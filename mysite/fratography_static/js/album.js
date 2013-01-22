3944f65491435c9ed23ad03d1e66fb4f144ec724
Devasia Joseph <devasia@area-52.(none)>
2013-01-22 22:57:03 +0530

    Album scroll error fix

--- mysite/fratography_static/js/album.js
+++ mysite/fratography_static/js/album.js
@@ -233,7 +233,7 @@ function steerImage(direction){
 
 function displayImage(image, id){
     $("#lightbox-test").trigger('close');
-    $("#image-display-container").html('<img src="'+image+'" id="display-image" onload="adjustImageModal()" style="width:100%">');
+    $("#image-display-container").html('<img src="'+image+'" id="display-image"  style="width:100%">');
     $("#lightbox-test").lightbox_me();
     $("#id_selected_image").val(id);
 }
